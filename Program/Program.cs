d4cb46082a7877260600e4971b4a184e299f1f12
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-05-22 15:45:01 +0900

    (2024.5.22) 클래스

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,16 +5,13 @@ using System.Windows.Markup;
 
 namespace Program
 {
-    #region 클래스
-    //사용자 정의 데이터 유형으로 속성과 함수가
+
     internal class Program
-    {      
-        // Main 함수는 프로그램의 진입점 역할을 수행하는 함수입니다.
+    {              
         static void Main(string[] args)
         {
-
-
+            
         }
     }
-        #endregion
+ 
 }
